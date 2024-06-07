bb7722a4a46474cf3bd1f97c138cc38e291beeb9
Herbeth <herbeth@hps.net.br>
2024-06-07 15:53:58 -0300

    improvement

--- build/apache2/entrypoint.sh
+++ build/apache2/entrypoint.sh
@@ -10,11 +10,12 @@ service php7.2-fpm start
 service php7.3-fpm start
 service php7.4-fpm start
 service php8.0-fpm start
+service php8.1-fpm start
 service php8.2-fpm start
 service php8.3-fpm start
 
 service apache2 restart
-service webmin start
+service webmin restart
 
 timedatectl set-timezone America/Sao_Paulo
 
